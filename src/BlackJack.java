47dec5c674542275ecc31f6d7b3d68e8d1e16579
Reid Brown <56902716+reidbrown1@users.noreply.github.com>
2021-11-29 23:26:44 -0500

    finalized bj game

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -13,6 +13,7 @@ public class BlackJack {
         int playerCard1, playerCard2, houseCard1, houseCard2, card;
         int result;
         char response;
+        boolean gameOver = false;
 
 
         int[][] deck = new int[4][13];
@@ -42,28 +43,74 @@ public class BlackJack {
         }
         response = scnr.next().charAt(0);
         while (true) {
-            if (response == 'h') {
+            if (response == 'h' && playerTotal < 21) {
                 card = game.returnActualCard(deck);
                 playerTotal += game.cardBJValue(card);
                 result = game.seeIfResult(playerTotal);
                 if (result == 1) {
-                    System.out.println("You got a " + card + "you now have " + playerTotal);
+                    System.out.println("You got a " + card + " you now have " + playerTotal);
                     System.out.println("Type h for hit or s for stay");
                     response = scnr.next().charAt(0);
                 } else if (result == 2) {
                     System.out.println("You busted with " + playerTotal + ". Dealer Wins");
-                    System.out.println("Would you like to play again?");
-                    response = scnr.next().charAt(0);
-
+                    gameOver = true;
                 } else if (result == 3) {
-                    System.out.println("21");
-                    // technically could be situation where dealer has bj as well but will deal with that later
-                    System.out.println("would you like to play again?");
+                    System.out.println("You have 21");
                 }
             } else if (response == 's') {
-                // dealers turn
-                // reveals card
-                // will continue hitting until
+                System.out.println("The dealers second card was a " + houseCard2 + " and has a total of " + houseTotal);
+                while (houseTotal < 17) {
+                    card = game.returnActualCard(deck);
+                    houseTotal += game.cardBJValue(card);
+                    System.out.println("house got " + card + " and now has " + houseTotal);
+                }
+                if (houseTotal <= 21) {
+                    if (houseTotal > playerTotal) {
+                        System.out.println("House wins");
+                        gameOver = true;
+                    } else if (houseTotal == playerTotal) {
+                        System.out.println("Push");
+                        gameOver = true;
+                    } else {
+                        System.out.println("Player Wins");
+                        gameOver = true;
+                    }
+                } else {
+                    System.out.println("House busts, player wins");
+                    gameOver = true;
+                }
+            }
+            if (gameOver) {
+                System.out.println("Would you like to play again? Type y or n");
+                response = scnr.next().charAt(0);
+                if (response == 'y') {
+                    game.resetDeck(deck);
+                    playerTotal = 0;
+                    houseTotal = 0;
+                    // begin game, will make into method in a little
+                    houseCard1 = game.returnActualCard(deck);
+                    houseTotal += game.cardBJValue(houseCard1);
+                    playerCard1 = game.returnActualCard(deck); // first card
+                    playerTotal += game.cardBJValue(playerCard1);
+                    houseCard2 = game.returnActualCard(deck);
+                    houseTotal += game.cardBJValue(houseCard2);
+                    playerCard2 = game.returnActualCard(deck); // second card
+                    playerTotal += game.cardBJValue(playerCard2);
+                    gameOver = false;
+
+                    System.out.println("player has: " + playerCard1 + ", " + playerCard2 + " with a total of " + playerTotal);
+                    System.out.println("house is showing " + houseCard1);
+                    result = game.seeIfResult(playerTotal);
+                    if (result == 1) {
+                        System.out.println("Type h for hit or s for stay");
+                    } else if (result == 3) {
+                        System.out.println("Blackjack!");
+                        // technically could be situation where dealer has bj as well but will deal with that later
+                    }
+                    response = scnr.next().charAt(0);
+                } else if (response == 'n') {
+                    break;
+                }
             }
         }
 
